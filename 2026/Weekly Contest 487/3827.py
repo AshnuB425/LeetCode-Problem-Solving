01d3069d910b6daeb409ce3c6e45338378922d43
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 12:04:46 -0400

    Implement brute force and bit manipulation approaches
    Added a brute force approach to count monobit integers.

--- 2026/Weekly Contest 487/3827.py
+++ 2026/Weekly Contest 487/3827.py
@@ -1,3 +1,13 @@
+#Approach 1: Brute Force
+class Solution:
+    def countMonobit(self, n: int) -> int:
+        c=0
+        for i in range(n+1):
+            if i&(i+1)==0:
+                c+=1
+        return c
+
+#Approach 2: Bit Manipulation
 class Solution:
     def countMonobit(self, n: int) -> int:
         return (n+1).bit_length()
