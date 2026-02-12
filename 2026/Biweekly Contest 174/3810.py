9851ba019c562b76628200a14ca136b313c7baed
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:32:28 -0400

    Add minOperations method to Solution class
    Implement minOperations method to count unique mismatches.

---
+++ 2026/Biweekly Contest 174/3810.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minOperations(self, nums: List[int], target: List[int]) -> int:
+        s=set()
+        for i in range(len(nums)):
+            if nums[i]!=target[i]:
+                if nums[i] not in s:
+                    s.add(nums[i])
+        return len(s)
