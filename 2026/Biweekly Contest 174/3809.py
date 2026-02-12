289a5fc0917f4894d55484e7734fafe3a84616ff
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:32:10 -0400

    Implement bestTower method to find optimal tower

---
+++ 2026/Biweekly Contest 174/3809.py
@@ -0,0 +1,14 @@
+class Solution:
+    def bestTower(self, towers: List[List[int]], center: List[int], radius: int) -> List[int]:
+        c=[1e9,1e9]
+        maxi=0
+        for i in range(len(towers)):
+            dis=abs(towers[i][0]-center[0])+abs(towers[i][1]-center[1])
+            if dis<=radius:
+                maxi=max(maxi,towers[i][2])
+        for i in range(len(towers)):
+            dis=abs(towers[i][0]-center[0])+abs(towers[i][1]-center[1])
+            if dis<=radius:
+                if towers[i][2]==maxi:
+                    c=min(c,[towers[i][0],towers[i][1]])
+        return [-1,-1] if c==[1e9,1e9] else c
