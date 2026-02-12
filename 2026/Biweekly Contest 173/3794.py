7a04d849b3fe9a8e9e880381b524d242b68bf72e
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 15:22:10 -0400

    Implement second approach for reversePrefix function
    Added a second approach using two pointers to reverse the prefix of a string.

--- 2026/Biweekly Contest 173/3794.py
+++ 2026/Biweekly Contest 173/3794.py
@@ -1,3 +1,15 @@
+#Approach 1: String Slicing
 class Solution:
     def reversePrefix(self, s: str, k: int) -> str:
         return s[:k][::-1]+s[k:]
+
+#Approach 2: 2 Pointers
+class Solution:
+    def reversePrefix(self, s: str, k: int) -> str:
+        i,j=0,k-1
+        s=list(s)
+        while i<j:
+            s[i],s[j]=s[j],s[i]
+            i+=1
+            j-=1
+        return "".join(s)
