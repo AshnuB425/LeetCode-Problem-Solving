cecf0a3391eab85c7c85ddba4f237f23aeb4b748
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:34:58 -0400

    Add minLength method to Solution class
    Implement minLength method to find minimum subarray length.

---
+++ 2026/Biweekly Contest 173/3795.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minLength(self, nums: List[int], k: int) -> int:
+        mini=1e9
+        d=[0]*(max(nums)+1)
+        j=0
+        s=0
+        for i in range(len(nums)):
+            d[nums[i]]+=1
+            if d[nums[i]]==1:
+                s+=nums[i]
+            while s>=k:
+                mini=min(mini,i-j+1)
+                d[nums[j]]-=1
+                if d[nums[j]]==0:
+                    s-=nums[j]
+                j+=1
+        return -1 if mini==1e9 else mini
