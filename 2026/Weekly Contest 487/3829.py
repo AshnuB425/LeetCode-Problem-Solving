fae31b23f734fb5cfa3780e8d2b3fb8613ccfd94
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:24:52 -0400

    Add RideSharingSystem class with core functionality
    Implement RideSharingSystem with methods to add riders and drivers, match them, and cancel riders.

---
+++ 2026/Weekly Contest 487/3829.py
@@ -0,0 +1,38 @@
+class RideSharingSystem:
+
+    def __init__(self):
+        self.a=[]
+        self.b=[]
+        self.e=dict()
+        self.i=0
+        self.j=0
+
+    def addRider(self, riderId: int) -> None:
+        self.a.append(riderId)
+
+    def addDriver(self, driverId: int) -> None:
+        self.b.append(driverId)
+
+    def matchDriverWithRider(self) -> List[int]:
+        if self.i<len(self.a) and self.j<len(self.b):
+            c=self.a[self.i]
+            d=self.b[self.j]
+            self.e[c]=1
+            self.i+=1
+            self.j+=1
+            return [d,c]
+        else:
+            return [-1,-1]
+
+    def cancelRider(self, riderId: int) -> None:
+        if riderId in self.a:
+            if riderId not in self.e:
+                self.a.remove(riderId)
+
+
+# Your RideSharingSystem object will be instantiated and called as such:
+# obj = RideSharingSystem()
+# obj.addRider(riderId)
+# obj.addDriver(driverId)
+# param_3 = obj.matchDriverWithRider()
+# obj.cancelRider(riderId)
