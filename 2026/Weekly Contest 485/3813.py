9dd0b102c0142cef17fc3de80eacf5247cd1488b
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 12:36:45 -0400

    Fix indentation and formatting issues in vowelConsonantScore

--- 2026/Weekly Contest 485/3813.py
+++ 2026/Weekly Contest 485/3813.py
@@ -1,3 +1,4 @@
+#Approach 1: String Manipulation
 class Solution:
     def vowelConsonantScore(self, s: str) -> int:
         sc=0
@@ -5,7 +6,8 @@ class Solution:
         for i in s:
             if i in 'aeiou':
                 v+=1
-            elif not i.isspace() and not i.isdigit():c+=1
+            elif not i.isspace() and not i.isdigit():
+                c+=1
         if c==0:
             return 0
         return v//c
