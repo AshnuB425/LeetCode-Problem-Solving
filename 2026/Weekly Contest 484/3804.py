38fe0f1c514592eb8802047d57176c5b372509d2
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:29:31 -0400

    Add centeredSubarrays method to Solution class
    Implement centeredSubarrays method to count centered subarrays.

---
+++ 2026/Weekly Contest 484/3804.py
@@ -0,0 +1,12 @@
+class Solution:
+    def centeredSubarrays(self, nums: List[int]) -> int:
+        c=0
+        for i in range(len(nums)):
+            a=set()
+            s=0
+            for j in range(i,len(nums)):
+                a.add(nums[j])
+                s+=nums[j]
+                if s in a:
+                    c+=1
+        return c
