c83cba8b7d3836e291a10c90abe74b646a411cab
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:28:18 -0400

    Create 3813.py

---
+++ 2026/Weekly Contest 485/3813.py
@@ -0,0 +1,11 @@
+class Solution:
+    def vowelConsonantScore(self, s: str) -> int:
+        sc=0
+        v,c=0,0
+        for i in s:
+            if i in 'aeiou':
+                v+=1
+            elif not i.isspace() and not i.isdigit():c+=1
+        if c==0:
+            return 0
+        return v//c
