5cea8bb1356eea3ec291e53f8b27997d83a39e2e
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:35:46 -0400

    Implement maximumScore function in Solution class

---
+++ 2025/Weekly Contest 482/3788.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumScore(self, nums: List[int]) -> int:
+        pre=[]
+        su=0
+        for i in nums:
+            su+=i
+            pre.append(su)
+        sf=[0]*len(nums)
+        mini=3e15
+        for i in range(len(nums)-1,-1,-1):
+            mini=min(mini,nums[i])
+            sf[i]=mini
+        maxi=-3e15
+        for i in range(len(nums)-1):
+            maxi=max(maxi,pre[i]-sf[i+1])
+        return maxi
