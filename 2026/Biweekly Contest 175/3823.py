cd3d506fa7d4159dcce6620570d5f7c182ea2135
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:25:44 -0400

    Create 3823.py

---
+++ 2026/Biweekly Contest 175/3823.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseByType(self, s: str) -> str:
+        spe=[]
+        nons=[]
+        s=list(s)
+        for i in s:
+            if i.isalpha():
+                spe.append(i)
+            else:
+                nons.append(i)
+        a,b=len(spe)-1,len(nons)-1
+        for i in range(len(s)):
+            if s[i].isalpha():
+                s[i]=spe[a]
+                a-=1
+            else:
+                s[i]=nons[b]
+                b-=1
+        return "".join(s)
