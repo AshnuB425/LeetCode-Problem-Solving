da6385b3051520772d6cd15de93b199da0d337c1
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:26:49 -0400

    Create 3818.py

---
+++ 2026/Weekly Contest 486/3818.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumPrefixLength(self, nums: List[int]) -> int:
+        mini=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i+1]>nums[i]:
+                mini=min(mini,i)
+            else:
+                break
+        return mini
