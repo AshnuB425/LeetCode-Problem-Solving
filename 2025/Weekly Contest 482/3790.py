abb7a47b74bebe86bc0243b3b39daab35c7ca417
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:36:26 -0400

    Add Solution class with minAllOneMultiple method

---
+++ 2025/Weekly Contest 482/3790.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minAllOneMultiple(self, k: int) -> int:
+        rem=0
+        for i in range(k):
+            rem=(rem*10+1)%k
+            if rem==0:
+                return i+1
+        return -1
