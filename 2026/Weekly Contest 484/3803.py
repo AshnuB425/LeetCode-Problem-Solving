ac3e1717a775f79a61cb1abc812da0e9b8818e7c
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:29:04 -0400

    Create 3803.py

---
+++ 2026/Weekly Contest 484/3803.py
@@ -0,0 +1,9 @@
+class Solution:
+    def residuePrefixes(self, s: str) -> int:
+        a=set()
+        c=0
+        for i in range(len(s)):
+            a.add(s[i])
+            if len(a)==(i+1)%3:
+                c+=1
+        return c
