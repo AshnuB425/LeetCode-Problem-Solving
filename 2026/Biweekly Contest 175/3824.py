c016c74100ca3882b3e61845af6d05705f4639cd
Ashnu B <85802362+AshnuB425@users.noreply.github.com>
2026-02-12 11:26:06 -0400

    Implement minimumK function in Solution class

---
+++ 2026/Biweekly Contest 175/3824.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumK(self, nums: List[int]) -> int:
+        i,j=1,100005
+        while i<=j:
+            m=(i+j)//2
+            ans=0
+            for k in range(len(nums)):
+                ans+=((nums[k]-1)//m)+1
+            if ans<=(m*m):
+                j=m-1
+            else:
+                i=m+1
+        return i
